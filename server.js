88c3a223e9b03afbd9674917bfe55ffe3e9957da
Jaime Pillora <jpillora@gmail.com>
2013-09-11 13:23:26 +1000

    updated

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ var http = require("http");
 var port = process.env.PORT || 4000;
 var total = 0;
 var live = 0;
-var proxyFile = new Buffer('<script src="http://jpillora.com/xdomain/dist/0/xdomain.js" master="*"></script>');
+var proxyFile = new Buffer('<script src="http://jpillora.com/xdomain/dist/0.5/xdomain.js" master="*"></script>');
 
 http.createServer(function (req, res) {
   total++;
   live++;
 
+  var status = 200;
+  if(/\/status\/(\d+)/.test(req.url))
+    status = parseInt(RegExp.$1, 10);
+
   if(req.url === '/proxy.html') {
     res.writeHead(200, {'Content-Type':'text/html'});
     res.end(proxyFile);
     return;
+  } else {
+    res.writeHead(status, {'Content-Type':'application/json'});
   }
 
   var data = {
@@ -33,8 +39,16 @@ http.createServer(function (req, res) {
   });
 
   req.on('end', function() {
-    res.end(JSON.stringify(data, null, 2));
-    live--;
+
+    var delay = 0;
+    if(/\/delay\/(\d+)/.test(req.url))
+      delay = parseInt(RegExp.$1, 10);
+
+    setTimeout(function() {
+      res.end(JSON.stringify(data, null, 2));
+      live--;
+    }, delay);
+
   });
 
 }).listen(port, function() {
