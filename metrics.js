9c1e6a7408702acd6f28f1335622ea82cb8846ae
Jaime Pillora <dev@jpillora.com>
2014-09-25 16:23:52 +1000

    auto-formatted tabs, minor fixes

--- metrics.js
+++ metrics.js
@@ -1,11 +1,9 @@
-
 var querystring = require('querystring');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-
 var Echo = mongoose.model('Echo', {
 	ip: String,
-	domains: [String],
+	domain: String,
 	method: String,
 	url: String,
 	body: String,
@@ -13,13 +11,12 @@ var Echo = mongoose.model('Echo', {
 	userAgent: String,
 	referer: String
 });
-
 var connecting = false;
 var disabled = false;
 
 function init() {
 	var uri = process.env.MONGOLAB_URI;
-	if(!uri) {
+	if (!uri) {
 		disabled = true;
 		return;
 	}
@@ -34,75 +31,62 @@ function init() {
 }
 
 function msg2doc(msg) {
-	//force domains to be an array
-	var domains = [];
-	if(msg.domains)
-		if(msg.domains instanceof Array)
-			domains = msg.domains;
-		else
-			domains.push(msg.domains);
+	//force domains to be a string
+	var domains = msg.domains;
+	if (domains && domains instanceof Array) domains = domains.join(",");
 	return {
 		ip: msg.ip,
 		domains: domains,
 		method: msg.method,
 		url: msg.url,
-		body: msg.body,
+		body: msg.body.length < 1000 ? msg.body : msg.body.substr(0, 1000),
 		host: msg.headers.host,
 		userAgent: msg.headers["user-agent"],
 		referer: msg.headers.referer || msg.headers.origin
 	};
 }
-
 exports.process = function(msg) {
-	if(!connecting)
-		init();
-	if(disabled)
-		return;
+	if (!connecting) init();
+	if (disabled) return;
+	//dont insert load tests
+	if (/^load-tester/.test(msg.headers["user-agent"])) return;
 	//add to db
-	new Echo(msg2doc(msg)).save(function (err) {
+	new Echo(msg2doc(msg)).save(function(err) {
 		if (err) return console.error('error saving document');
 	});
 };
 
-
 function stringify(obj) {
 	var str = JSON.stringify(obj || null, 0, 2);
-	if(typeof str !== "string")
-		str = "{}";
+	if (typeof str !== "string") str = "{}";
 	return new Buffer(str);
 }
-
 exports.fetch = function(req, res) {
-	if(!connecting)
-		init();
-	if(disabled)
-		return res.end("???");
+	if (!connecting) init();
+	if (disabled) return res.end("<disabled>");
 	var query = querystring.parse(/\?(.+)/.test(req.url) ? RegExp.$1 : '');
-
 	try {
-		for(var k in query)
-			if(/^\/.+\/$/.test(k))
-				query[k] = new RegExp(k);
-	} catch(e) {
+		for (var k in query)
+			if (/^\/.+\/$/.test(k)) query[k] = new RegExp(k);
+	} catch (e) {
 		return res.end("regex error: " + e);
 	}
-
-	res.writeHead(200, { "content-type": "application/json" });
+	res.writeHead(200, {
+		"content-type": "application/json"
+	});
 	var obj = null;
 	res.write("[");
-	Echo.find(query).stream()
-		.on('data', function (doc) {
-			if(obj !== null)
-				res.write(stringify(obj) + ",");
-			obj = doc.toObject();
-			for(var k in obj)
-				if(k[0] === "_")
-					delete obj[k];
-		}).on('error', function (err) {
-			res.write(JSON.stringify({ err: err.toString() }, 0, 2));
-		}).on('close', function () {
-			if(obj !== null)
-				res.write(stringify(obj));
-			res.write("]");
-		});
+	Echo.find(query).limit(1000).stream().on('data', function(doc) {
+		if (obj !== null) res.write(stringify(obj) + ",");
+		obj = doc.toObject();
+		for (var k in obj)
+			if (k[0] === "_") delete obj[k];
+	}).on('error', function(err) {
+		res.write(JSON.stringify({
+			err: err.toString()
+		}, 0, 2));
+	}).on('close', function() {
+		if (obj !== null) res.write(stringify(obj));
+		res.write("]");
+	});
 };
--- server.js
+++ server.js
@@ -6,202 +6,173 @@ var port = process.env.PORT || parseInt(process.argv[2], 10) || 4000;
 var echoes = [];
 var total = 0;
 var live = 0;
-
+var up = new Date().toUTCString();
+//replay a single echo
 var getEcho = function(id, res) {
-  res.writeHead(200, {'Content-Type':'application/json'});
-  res.end(JSON.stringify(echoes[id], null, 2));
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+	res.end(JSON.stringify(echoes[id], null, 2));
 };
-
+//get metadata for all echoes
 var getEchos = function(res) {
-  res.writeHead(200, {'Content-Type':'application/json'});
-  var ips = {};
-  var methods = {};
-  var stats = {
-    live: live,
-    total: total,
-    totalLength: 0,
-    methods:methods,
-    ips:ips
-  };
-  echoes.forEach(function(echo) {
-
-    var id = echo.domains || echo.ip;
-
-    if(!ips[id]) ips[id] = [];
-    ips[id].push(echo.meta.num);
-
-    if(!methods[echo.method]) methods[echo.method] = 0;
-    methods[echo.method]++;
-
-    stats.totalLength += parseInt(echo.headers['content-length'],10) || 0;
-  });
-
-  //compress id lists
-  for(var id in ips) {
-    var range = null;
-    ips[id] = ips[id].reduce(function(nums, num, i, arr) {
-
-      var lastIndex = arr.length-1;
-      var hasNext = i+1 <= lastIndex;
-      var increments = hasNext && arr[i+1] === num+1;
-
-      if(hasNext && increments) {
-        if(!range)
-          range = [num];
-        return nums;
-      } else if(range) {
-        range.push(num);
-        nums.push(range);
-        range = null;
-        return nums;
-      }
-      nums.push(num);
-      return nums;
-    }, []);
-  }
-
-  res.end(JSON.stringify(stats), null, 2);
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+	var ips = {};
+	var methods = {};
+	var stats = {
+		live: live,
+		total: total,
+		totalLength: 0,
+		methods: methods,
+		ips: ips
+	};
+	echoes.forEach(function(echo) {
+		var id = echo.domains || echo.ip;
+		if (!ips[id]) ips[id] = [];
+		ips[id].push(echo.meta.num);
+		if (!methods[echo.method]) methods[echo.method] = 0;
+		methods[echo.method]++;
+		stats.totalLength += parseInt(echo.headers['content-length'], 10) || 0;
+	});
+	//compress id lists
+	for (var id in ips) {
+		var range = null;
+		ips[id] = ips[id].reduce(function(nums, num, i, arr) {
+			var lastIndex = arr.length - 1;
+			var hasNext = i + 1 <= lastIndex;
+			var increments = hasNext && arr[i + 1] === num + 1;
+			if (hasNext && increments) {
+				if (!range) range = [num];
+				return nums;
+			} else if (range) {
+				range.push(num);
+				nums.push(range);
+				range = null;
+				return nums;
+			}
+			nums.push(num);
+			return nums;
+		}, []);
+	}
+	res.end(JSON.stringify(stats), null, 2);
 };
-
+//special endpoint for xdomain
 var getProxy = function(xdomain, res) {
-  res.writeHead(200, {'Content-Type':'text/html'});
-  res.end('<!DOCTYPE HTML>\n'+
-          '<script src="'+(xdomain || '//rawgit.com/jpillora/xdomain/gh-pages/dist/0.6/xdomain.js')+'" master="*"></script>');
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	res.end('<!DOCTYPE HTML>\n' + '<script src="' + (xdomain || '//rawgit.com/jpillora/xdomain/gh-pages/dist/0.6/xdomain.js') + '" master="*"></script>');
 };
+//create the server!
+http.createServer(function(req, res) {
+	//special actions
+	if (req.url === '/favicon.ico') {
+		return res.end();
+	} else if (/^\/echo\/(\d+)\/?/.test(req.url)) {
+		return getEcho(RegExp.$1, res);
+	} else if (/^\/echoes-request/.test(req.url)) {
+		return metrics.fetch(req, res);
+	} else if (/^\/echoes/.test(req.url)) {
+		return getEchos(res);
+	} else if (/^\/proxy\.html(\?src=(.+))?$/.test(req.url)) {
+		return getProxy(RegExp.$2, res);
+	}
+	//do echo
+	var num = echoes.length;
+	var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+	var status = 200;
+	if (/\/status\/(\d{3})/.test(req.url)) status = parseInt(RegExp.$1, 10);
+	var delay = 0;
+	if (/\/delay\/(\d{1,5})/.test(req.url)) delay = parseInt(RegExp.$1, 10);
+	var host = 'http://' + req.headers['host'];
+	var referer = /^(https?:\/\/[^\/]+)/.test(req.headers['referer']) ? RegExp.$1 : '*';
+	var headers = {
+		'content-type': 'application/json',
+		'echo-server-version': pkg.version,
+		'cache-control': 'no-cache'
+	};
+	//apply cors when needed
+	if (host !== referer) {
+		headers['access-control-allow-credentials'] = true;
+		headers['access-control-allow-origin'] = referer;
+		if (req.headers['access-control-request-method']) headers['access-control-allow-methods'] = req.headers['access-control-request-method'];
+		if (req.headers['access-control-request-headers']) headers['access-control-allow-headers'] = req.headers['access-control-request-headers'];
+		headers['access-control-max-age'] = 0;
+	}
+	total++;
+	live++;
+	//wipe heroku headers
+	for (var k in req.headers)
+		if (/^x-/.test(k)) delete req.headers[k];
+	var data = {
+		ip: ip,
+		domains: null, //unset
+		time: Date.now(),
+		method: req.method,
+		url: req.url,
+		body: "",
+		headers: req.headers,
+		meta: {
+			up: up,
+			num: num,
+			live: live,
+			status: status,
+			delay: delay
+		}
+	};
+	//echoes fall off the front
+	if (echoes.length >= 100) echoes.unshift();
+	//push onto the back
+	echoes.push(data);
+	req.on('data', function(buffer) {
+		data.body += buffer.toString();
+	});
+	var send = function() {
+		//send after has domains AND request ended
+		if (data.domains === null || !req.ended) return;
+		//process (if able)
+		metrics.process(data);
+		var buff = new Buffer(JSON.stringify(data, null, 2));
+		var length = buff.length;
+		headers['content-length'] = length;
+		//write header
+		res.writeHead(status, headers);
 
-http.createServer(function (req, res) {
-
-  //special actions
-  if(req.url === '/favicon.ico') {
-    return res.end();
-  } else if(/^\/echo\/(\d+)\/?/.test(req.url)) {
-    return getEcho(RegExp.$1, res);
-  } else if(/^\/echoes-request/.test(req.url)) {
-    return metrics.fetch(req, res);
-  } else if(/^\/echoes/.test(req.url)) {
-    return getEchos(res);
-  } else if(/^\/proxy\.html(\?src=(.+))?$/.test(req.url)) {
-    return getProxy(RegExp.$2, res);
-  }
-
-  //do echo
-  var num = echoes.length;
-  var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-  var status = 200;
-  if(/\/status\/(\d{3})/.test(req.url))
-    status = parseInt(RegExp.$1, 10);
-
-  var delay = 0;
-  if(/\/delay\/(\d{1,5})/.test(req.url))
-    delay = parseInt(RegExp.$1, 10);
-  
-  var host = 'http://'+req.headers['host'];
-  var referer = /^(https?:\/\/[^\/]+)/.test(req.headers['referer']) ? RegExp.$1 : '*';
-  var headers = {
-    'content-type':'application/json',
-    'echo-server-version': pkg.version,
-    'cache-control':'no-cache'
-  };
-
-  //apply cors when needed
-  if(host !== referer) {
-    headers['access-control-allow-credentials'] = true;
-    headers['access-control-allow-origin'] = referer;
-    if(req.headers['access-control-request-method'])
-      headers['access-control-allow-methods'] = req.headers['access-control-request-method'];
-    if(req.headers['access-control-request-headers'])
-      headers['access-control-allow-headers'] = req.headers['access-control-request-headers'];
-    headers['access-control-max-age'] = 0;
-  }
-
-  total++;
-  live++;
-  
-  //wipe heroku headers
-  for(var k in req.headers)
-    if(/^x-/.test(k))
-      delete req.headers[k];
-
-  var data = {
-    ip: ip,
-    domains: null, //unset
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    body: "",
-    headers: req.headers,
-    meta: {
-      num: num,
-      live: live,
-      status: status,
-      delay: delay
-    }
-  };
-
-  //echoes fall off the front
-  if(echoes.length >= 100)
-    echoes.unshift();
-  //push onto the back
-  echoes.push(data);
-
-  req.on('data', function(buffer) {
-    data.body += buffer.toString();
-  });
-
-  var send = function() {
-    //send after has domains AND request ended
-    if(data.domains === null || !req.ended)
-      return;
-
-    //process (if able)
-    metrics.process(data);
-
-    var buff = new Buffer(JSON.stringify(data, null, 2));    
-    var length = buff.length;
-    headers['content-length'] = length;
-    //write header
-    res.writeHead(status, headers);
-    
-    function end(buff) {
-      if(buff)
-        res.write(buff);
-      res.end();
-      live--;
-    }
-
-    //write all now
-    if(delay === 0)
-      return end(buff);
-    
-    //with delay, write out 10 chunks over 10 time intervals
-    var d = Math.ceil(delay/10);
-    var l = Math.ceil(length/10);
-
-    (function sendChunk() {
-      var b = buff.slice(0, l);
-      if(b.length === 0)
-        return end();
-      res.write(b);
-      buff = buff.slice(l);
-      setTimeout(sendChunk, d);
-    }());
-  };
-
-  dns.reverse(ip, function(err, domains) {
-    data.domains =
-      domains ?
-        domains.length === 1 ? domains[0] : 
-        domains.length >=  2 ? domains : undefined
-      : undefined;
-    send();
-  });
-
-  req.on('end', function() {
-    req.ended = true;
-    send();
-  });
-
+		function end(buff) {
+			if (buff) res.write(buff);
+			res.end();
+			live--;
+		}
+		//write all now
+		if (delay === 0) return end(buff);
+		//with delay, write out 10 chunks over 10 time intervals
+		var d = Math.ceil(delay / 10);
+		var l = Math.ceil(length / 10);
+		(function sendChunk() {
+			var b = buff.slice(0, l);
+			if (b.length === 0) return end();
+			res.write(b);
+			buff = buff.slice(l);
+			setTimeout(sendChunk, d);
+		}());
+	};
+	//attempt a reverse lookup on the remote ip
+	try {
+		dns.reverse(ip, function(err, domains) {
+			data.domains = domains ? domains.length === 1 ? domains[0] : domains.length >= 2 ? domains : undefined : undefined;
+			send();
+		});
+	} catch (err) {
+		data.domains = undefined;
+		send();
+	}
+	//
+	req.on('end', function() {
+		req.ended = true;
+		send();
+	});
 }).listen(port, "0.0.0.0", function() {
-  console.log("listening on "+port+"...");
+	console.log("listening on " + port + "...");
 });
